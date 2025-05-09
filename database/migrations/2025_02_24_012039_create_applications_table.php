6cb649a74cbe701c83bd5e5b1051e2e6a15930e3
gyot <gyotdoank@gmail.com>
2025-05-09 09:20:10 +0800

    Perbaikan koneksi database

--- database/migrations/2025_02_24_012039_create_applications_table.php
+++ database/migrations/2025_02_24_012039_create_applications_table.php
@@ -30,6 +30,9 @@ return new class extends Migration
      */
     public function down(): void
     {
+        Schema::disableForeignKeyConstraints();
         Schema::dropIfExists('applications');
+        Schema::enableForeignKeyConstraints();
+
     }
 };
--- database/migrations/2025_04_07_114558_approvals.php
+++ database/migrations/2025_04_07_114558_approvals.php
@@ -29,5 +29,8 @@ return new class extends Migration
     public function down(): void
     {
         //
+        Schema::disableForeignKeyConstraints();
+        Schema::dropIfExists('approvals');
+        Schema::enableForeignKeyConstraints();
     }
 };
